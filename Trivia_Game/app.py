0aef1624d3c2521dd6c659b5259f4145cc6c54ac
Marcin Chlebek <marcinchlebek@gmail.com>
2024-03-18 21:02:48 +0100

    trivia second commit

--- Trivia_Game/app.py
+++ Trivia_Game/app.py
@@ -7,6 +7,17 @@ from PyQt5.QtGui import QCursor
 #function imports
 #from functions import frame1, frame2, frame3, frame4, grid
 
+widgets = {
+    "logo": [],
+    "button": [],
+    "score": [],
+    "question": [],
+    "answer1": [],
+    "answer2": [], 
+    "answer3": [], 
+    "answer4": [],        
+    }
+
 #initiallize GUI application
 app = QApplication(sys.argv)
 
@@ -17,14 +28,114 @@ window.setStyleSheet("background: #161219;")
 
 grid = QGridLayout()
 
-#display Logo
-image = QPixmap("trivia.png")
-logo = QLabel()
-logo.setPixmap(image)
-logo.setAlignment(QtCore.Qt.AlignCenter) 
-logo.setStyleSheet("margin-top: 100px;")
+def create_button(answer, l_margin, r_margin):
+    button = QPushButton(answer)
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setFixedWidth(485)
+    button.setStyleSheet(
+    #setting variable margins
+        "*{margin-left: " + str(l_margin) +"px;"+
+        "margin-right: " + str(r_margin) +"px;"+
+        '''
+        border: 4px solid '#BC006C';
+        color: white;
+        font-family: 'shanti';
+        font-size: 16px;
+        border-radius: 25px;
+        padding: 15px 0;
+        margin-top: 20px;
+        }
+        *:hover{
+            background: '#BC006C';
+        }
+        '''
+    )
+    return button
 
-grid.addWidget(logo, 0, 0)
+def frame1():
+    #display Logo
+    image = QPixmap("trivia.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter) 
+    logo.setStyleSheet("margin-top: 100px;")
+    widgets["logo"].append(logo)
+
+    #button widget
+    button = QPushButton("PLAY")
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setStyleSheet(
+        "*{border: 4px solid '#BC006C';" +
+        "border-radius: 45px;" +
+        "font-size: 35px;" +
+        "color: 'white';" +
+        "padding: 25px 0;" +
+        "margin: 100px 200px;}" +
+        "*:hover{background: '#BC006C';}"
+        )
+    widgets["button"].append(button)
+
+    grid.addWidget(widgets["logo"][-1], 0, 0)
+    grid.addWidget(widgets["button"][-1], 1, 0)
+
+def frame2():
+    score = QLabel("80")
+    score.setAlignment(QtCore.Qt.AlignRight) 
+    score.setAlignment(QtCore.Qt.AlignRight)
+    score.setStyleSheet(
+        '''
+        font-size: 35px;
+        color: 'white';
+        padding: 15px 10px;
+        margin: 20px 200px;
+        background: '#64A314';
+        border: 1px solid '#64A314';
+        border-radius: 35px;
+        '''
+    )
+    widgets["score"].append(score)
+
+    question = QLabel("Placeholder text")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        '''
+        font-family: 'shanti';
+        font-size: 25px;
+        color: 'white';
+        padding: 75px;
+        '''
+    )
+    widgets["question"].append(question)
+
+    button1 = create_button("answer1", 85, 5)
+    button2 = create_button("answer2", 5, 85)
+    button3 = create_button("answer3", 85, 5)
+    button4 = create_button("answer4", 5, 85)
+
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+    widgets["answer3"].append(button3)
+    widgets["answer4"].append(button4)
+
+    #display Logo
+    image = QPixmap("trivia.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter) 
+    logo.setStyleSheet("margin-top: 75px, margin-bottom: 30px;")
+    widgets["logo"].append(logo)
+
+
+    grid.addWidget(widgets["score"][-1], 0, 1)
+    grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+    grid.addWidget(widgets["answer3"][-1], 3, 0)
+    grid.addWidget(widgets["answer4"][-1], 3, 1)
+    grid.addWidget(widgets["logo"][-1], 4, 0, 1, 2)
+
+frame2()
 
 window.setLayout(grid)
 
