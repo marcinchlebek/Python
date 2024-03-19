ff96f77f5e09ff1b8e485fa090c63fdcaf54ae7a
Marcin Chlebek <marcinchlebek@gmail.com>
2024-03-19 12:31:44 +0100

    added API data base of questions

--- Trivia_Game/functions.py
+++ Trivia_Game/functions.py
@@ -5,3 +5,273 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 import random
+
+# https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple
+with urlopen("https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple") as webpage:
+    data = json.loads(webpage.read().decode())
+    df = pd.DataFrame(data["results"])
+    print(df.columns)
+
+def preload_data():
+    question = df["question"][0]
+    correct = df["correct_answer"][0]
+    wrong = df["incorrect_answers"][0]
+
+    parameters["question"].append(question)
+    parameters["correct"].append(correct)
+
+    all_answers = wrong + [correct]
+    random.shuffle(all_answers)
+    parameters["answer1"].append(all_answers[0])
+    parameters["answer2"].append(all_answers[1])
+    parameters["answer3"].append(all_answers[2])
+    parameters["answer4"].append(all_answers[3])
+
+
+parameters = {
+    "question": [],
+    "answer1": [],
+    "answer2": [],
+    "answer3": [],
+    "answer4": [],
+    "correct": [],
+}
+
+preload_data()
+
+widgets = {
+    "logo": [],
+    "button": [],
+    "score": [],
+    "question": [],
+    "answer1": [],
+    "answer2": [], 
+    "answer3": [], 
+    "answer4": [],        
+    }
+
+grid = QGridLayout()
+
+def clear_widgets():
+    for widget in widgets:
+        if widgets[widget] != []:
+            widgets[widget][-1].hide()
+        for i in range(0, len(widgets[widget])):
+            widgets[widget].pop()
+
+def show_frame1():
+    clear_widgets()
+    frame1()
+
+def start_game():
+    clear_widgets()
+    frame2()
+
+
+def create_button(answer, l_margin, r_margin):
+    button = QPushButton(answer)
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setFixedWidth(485)
+    button.setStyleSheet(
+    #setting variable margins
+        "*{margin-left: " + str(l_margin) +"px;"+
+        "margin-right: " + str(r_margin) +"px;"+
+        '''
+        border: 4px solid '#BC006C';
+        color: white;
+        font-family: 'shanti';
+        font-size: 16px;
+        border-radius: 25px;
+        padding: 15px 0;
+        margin-top: 20px;
+        }
+        *:hover{
+            background: '#BC006C';
+        }
+        '''
+    )
+    button.clicked.connect(show_frame1)
+    return button
+
+def frame1():
+    #display Logo
+    image = QPixmap("trivia.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter) 
+    logo.setStyleSheet("margin-top: 100px;")
+    widgets["logo"].append(logo)
+
+    #button widget
+    button = QPushButton("PLAY")
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+    button.setStyleSheet(
+        "*{border: 4px solid '#BC006C';" +
+        "border-radius: 45px;" +
+        "font-size: 35px;" +
+        "color: 'white';" +
+        "padding: 25px 0;" +
+        "margin: 100px 200px;}" +
+        "*:hover{background: '#BC006C';}"
+        )
+    button.clicked.connect(start_game)
+    widgets["button"].append(button)
+
+    grid.addWidget(widgets["logo"][-1], 0, 0, 1, 2)
+    grid.addWidget(widgets["button"][-1], 1, 0, 1, 2)
+
+def frame2():
+    score = QLabel("80")
+    score.setAlignment(QtCore.Qt.AlignRight) 
+    score.setAlignment(QtCore.Qt.AlignRight)
+    score.setStyleSheet(
+        '''
+        font-size: 35px;
+        color: 'white';
+        padding: 15px 10px;
+        margin: 20px 200px;
+        background: '#64A314';
+        border: 1px solid '#64A314';
+        border-radius: 35px;
+        '''
+    )
+    widgets["score"].append(score)
+
+    question = QLabel("Placeholder text")
+    question.setAlignment(QtCore.Qt.AlignCenter)
+    question.setWordWrap(True)
+    question.setStyleSheet(
+        '''
+        font-family: 'shanti';
+        font-size: 25px;
+        color: 'white';
+        padding: 75px;
+        '''
+    )
+    widgets["question"].append(question)
+
+    button1 = create_button("answer1", 85, 5)
+    button2 = create_button("answer2", 5, 85)
+    button3 = create_button("answer3", 85, 5)
+    button4 = create_button("answer4", 5, 85)
+
+    widgets["answer1"].append(button1)
+    widgets["answer2"].append(button2)
+    widgets["answer3"].append(button3)
+    widgets["answer4"].append(button4)
+
+    #display Logo
+    image = QPixmap("trivia.png")
+    logo = QLabel()
+    logo.setPixmap(image)
+    logo.setAlignment(QtCore.Qt.AlignCenter) 
+    logo.setStyleSheet("margin-top: 75px, margin-bottom: 30px;")
+    widgets["logo"].append(logo)
+
+
+    grid.addWidget(widgets["score"][-1], 0, 1)
+    grid.addWidget(widgets["question"][-1], 1, 0, 1, 2)
+    grid.addWidget(widgets["answer1"][-1], 2, 0)
+    grid.addWidget(widgets["answer2"][-1], 2, 1)
+    grid.addWidget(widgets["answer3"][-1], 3, 0)
+    grid.addWidget(widgets["answer4"][-1], 3, 1)
+    grid.addWidget(widgets["logo"][-1], 4, 0, 1, 2)
+
+def frame3():
+    #congradulations widget
+    message = QLabel("Congradulations! You\nare a true programmer!\n your score is:")
+    message.setAlignment(QtCore.Qt.AlignRight)
+    message.setStyleSheet(
+        "font-family: 'Shanti'; font-size: 25px; color: 'white'; margin: 100px 0px;"
+        )
+    widgets["message"].append(message)
+
+    #score widget
+    score = QLabel("100")
+    score.setStyleSheet("font-size: 100px; color: #8FC740; margin: 0 75px 0px 75px;")
+    widgets["score"].append(score)
+
+    #go back to work widget
+    message2 = QLabel("OK. Now go back to WORK.")
+    message2.setAlignment(QtCore.Qt.AlignCenter)
+    message2.setStyleSheet(
+        "font-family: 'Shanti'; font-size: 30px; color: 'white'; margin-top:0px; margin-bottom:75px;"
+        )
+    widgets["message2"].append(message2)
+
+    #button widget
+    button = QPushButton('TRY AGAIN')
+    button.setStyleSheet(
+        "*{background:'#BC006C'; padding:25px 0px; border: 1px solid '#BC006C'; color: 'white'; font-family: 'Arial'; font-size: 25px; border-radius: 40px; margin: 10px 300px;} *:hover{background:'#ff1b9e';}"
+        )
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+
+    widgets["button"].append(button)
+
+    #logo widget
+    pixmap = QPixmap('logo_bottom.png')
+    logo = QLabel()
+    logo.setPixmap(pixmap)
+    logo.setAlignment(QtCore.Qt.AlignCenter)
+    logo.setStyleSheet(
+        "padding :10px; margin-top:75px; margin-bottom: 20px;"
+    )
+    widgets["logo"].append(logo)
+
+    #place widgets on the grid
+    grid.addWidget(widgets["message"][-1], 2, 0)
+    grid.addWidget(widgets["score"][-1], 2, 1)
+    grid.addWidget(widgets["message2"][-1], 3, 0, 1, 2)
+    grid.addWidget(widgets["button"][-1], 4, 0, 1, 2)
+    grid.addWidget(widgets["logo"][-1], 5, 0, 2, 2)
+
+
+def frame4():
+    #sorry widget
+    message = QLabel("Sorry, this answer \nwas wrong\n your score is:")
+    message.setAlignment(QtCore.Qt.AlignRight)
+    message.setStyleSheet(
+        "font-family: 'Shanti'; font-size: 35px; color: 'white'; margin: 75px 5px; padding:20px;"
+        )
+    widgets["message"].append(message)
+
+    #score widget
+    score = QLabel("50")
+    score.setStyleSheet("font-size: 100px; color: white; margin: 0 75px 0px 75px;")
+    widgets["score"].append(score)
+
+    #button widget
+    button = QPushButton('TRY AGAIN')
+    button.setStyleSheet(
+        '''*{
+            padding: 25px 0px;
+            background: '#BC006C';
+            color: 'white';
+            font-family: 'Arial';
+            font-size: 35px;
+            border-radius: 40px;
+            margin: 10px 200px;
+        }
+        *:hover{
+            background: '#ff1b9e';
+        }'''
+        )
+    button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+
+    widgets["button"].append(button)
+
+    #logo widget
+    pixmap = QPixmap('logo_bottom.png')
+    logo = QLabel()
+    logo.setPixmap(pixmap)
+    logo.setAlignment(QtCore.Qt.AlignCenter)
+    logo.setStyleSheet(
+        "padding :10px; margin-top:75px;"
+    )
+    widgets["logo"].append(logo)
+
+    #place widgets on the grid
+    grid.addWidget(widgets["message"][-1], 1, 0)
+    grid.addWidget(widgets["score"][-1], 1, 1)
+    grid.addWidget(widgets["button"][-1], 2, 0, 1, 2)
+    grid.addWidget(widgets["logo"][-1], 3, 0, 1, 2)
